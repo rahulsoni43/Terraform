704994e18d3d3bf782c86a4ed47ab6cca7e3cb25
rahulsoni43 <41282873+rahulsoni43@users.noreply.github.com>
2020-03-15 18:22:24 +0530

    Update provider.tf

--- terraform-skill/provider.tf
+++ terraform-skill/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.aws_region
-  secret_key = "eCbX7t30Ll5vvHgVk+GZJSZpVn4SzbgW7SlcUg1K"
-  access_key = "AKIAVRPP7FFVQKTTYK5G"
+  secret_key = ""
+  access_key = ""
 }
