7eefde4094a74686103a134131aca07e12714cd3
rahulsoni43 <41282873+rahulsoni43@users.noreply.github.com>
2020-03-31 15:43:49 +0530

    Update packer.sh

--- terraform-jenkins/packer.sh
+++ terraform-jenkins/packer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
-cat terraform-jenkins/packer.json
-ARTIFACT=`packer build -machine-readable terraform-jenkins/packer.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+cd terraform-jenkins
+ARTIFACT=`packer build -machine-readable packer.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'`
 AMI_ID= `echo $ARTIFACT | awk -F ':' '{print $2}'`
 echo 'variable AMI_ID = "'${ARTIFACT}'"' > amivar.tf
 S3_BUCKET=`aws s3 ls | grep rahul | cut -d ' ' -f3`
